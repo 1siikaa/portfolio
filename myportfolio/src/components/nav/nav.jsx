a6298fa59075f31426fb6173e5409f3df54a12e4
1siikaa <srishtidwivedi672@gmail.com>
2023-02-06 03:14:26 +0530

    done

--- myportfolio/src/components/nav/nav.jsx
+++ myportfolio/src/components/nav/nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutLineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import
+
 
 function nav() {
   return (
-    <div>nav</div>
+    <nav>
+      <a href = "#"></a>
+      <a href = "#about"></a>
+      <a href = "#experience"></a>
+      <a href = "#services"></a>
+      <a href = "#contact"></a>
+    </nav>
   )
 }
 
