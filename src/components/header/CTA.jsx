cb9d366a7bcdc026d219b19be227295d08d92a73
Vanshika Dubey / <srishtidwivedi672@gmail.com>
2025-04-21 23:37:08 +0530

    portfolio-download-cv-updated

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ import CV from '../../assets/cv.pdf'
 function CTA() {
   return (
     <div className="cta">
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={CV} download="Vanshika SE.pdf" className='btn'>Download CV</a>
         <a href='https://www.linkedin.com/mwlite/in/vanshika-dubey-479693227'  target='_blank' rel="noreferrer"  className='btn btn-primary'>Let's Talk</a>
         
     </div>
