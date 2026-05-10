ed4441380a9bf4878985a09f1937cc56fe150e4b
Vanshika Dubey / <srishtidwivedi672@gmail.com>
2026-05-10 22:54:12 +0530

    view certificate and experience updated

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -96,6 +96,27 @@ function experience() {
 <small className='text-light'>Experienced</small>
 </div>
 </article>
+<article className='experience__details'>
+<BsPatchCheckFill className='experience__details-icon'/>
+<div>
+<h4>Confluence</h4>
+<small className='text-light'>Experienced</small>
+</div>
+</article>
+<article className='experience__details'>
+<BsPatchCheckFill className='experience__details-icon'/>
+<div>
+<h4>Google Sheet/ MS Excel</h4>
+<small className='text-light'>Experienced</small>
+</div>
+</article>
+<article className='experience__details'>
+<BsPatchCheckFill className='experience__details-icon'/>
+<div>
+<h4>TypeScript</h4>
+<small className='text-light'>Intermediate</small>
+</div>
+</article>
         </div>
       </div>
       </div>
